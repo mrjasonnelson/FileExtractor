9d399269c9494ae22ce42883afec0ebfc2f18ebd
mrjasonnelson <git@jason-nelson.co.uk>
2017-02-02 20:00:54 +0000

    Console: Formatting
    Formatting of comments and removal of commented code

--- FileExtractor/FileExtractor/Program.cs
+++ FileExtractor/FileExtractor/Program.cs
@@ -6,24 +6,24 @@ using System.Linq;
 
 namespace FileExtractor
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
             Console.WriteLine("File Extractor");
 
-            // get file with list of file names 
+            // Get file with list of file names 
             Console.WriteLine(Environment.NewLine + "List file:");
             var listFile = Console.ReadLine();
-            int lineCount = File.ReadLines(listFile).Count();
+            var lineCount = File.ReadLines(listFile).Count();
 
             Console.WriteLine(string.Concat(lineCount, " files to copy.", Environment.NewLine));
 
-            // get source directory 
+            // Get source directory 
             Console.WriteLine(string.Concat(Environment.NewLine, "Source Directory:"));
             var sourceDir = Console.ReadLine();
 
-            // get target directory 
+            // Get target directory 
             Console.WriteLine(string.Concat(Environment.NewLine, "Target Directory"));
             var targetDir = Console.ReadLine();
 
@@ -46,18 +46,13 @@ namespace FileExtractor
                 if (File.Exists(sourceDir + line))
                 {
                     File.Copy(sourceDir + line, targetDir + line, true);
-                    //Console.SetCursorPosition(0, Console.CursorTop - 1); 
-                    //Console.WriteLine("                                                                                               "); 
                     Console.SetCursorPosition(0, Console.CursorTop - 1);
                     Console.WriteLine(string.Concat(counter, "/", lineCount));
-                    //Console.WriteLine(string.Concat(counter, "/", lineCount, " | ", line, " copied.")); 
                     counter++;
                 }
                 else
                 {
                     missingFiles.Add(line);
-                    //Console.SetCursorPosition(0, Console.CursorTop - 1); 
-                    //Console.WriteLine(string.Concat(counter, "/", lineCount, " | ", line, " does not exist.")); 
                 }
             }
 
@@ -68,7 +63,7 @@ namespace FileExtractor
 
             Console.WriteLine(string.Format("{0}Job Complete!{0}{0}", Environment.NewLine));
 
-            // display a report of number of files copied and a list of missing files 
+            // Display a report of number of files copied and a list of missing files 
             Console.WriteLine("### Job Report ###" + Environment.NewLine);
 
             Console.WriteLine(string.Concat(
